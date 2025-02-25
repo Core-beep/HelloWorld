2dd561f13e29bdfbc0a3288233c0bdc6505a13a9
Core-beep <RumleAnton@hotmail.com>
2025-02-25 12:38:56 +0100

    idk01

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61516e2f799f4b63c1a51fb5fa4aeeba534aa80e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2543ab9c75377b9931a57cef4decd6c86d2f2168")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
